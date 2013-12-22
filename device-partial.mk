0d4b4a9130d9420f1718144f3513a8720e7263c7
android <trevd1234@gmail.com>
2013-12-22 22:00:30 +0000

    Initial commit for vendor/imgtec/a80sboard

---
+++ device-partial.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Imgtec blob(s) necessary for A80G9S hardware
+PRODUCT_PACKAGES += \
+	pvrsrvctl_SGX540_120 \
+	pvrsrvinit \
+	libEGL_POWERVR_SGX540_120 \
+	libGLESv1_CM_POWERVR_SGX540_120 \
+	libGLESv2_POWERVR_SGX540_120 \
+	gralloc.omap4 \
+	pvrsrvkm_sgx540_120 \
+	libglslcompiler_SGX540_120 \
+	libIMGegl_SGX540_120 \
+	libpvr2d_SGX540_120 \
+	libpvrANDROID_WSEGL_SGX540_120 \
+	libPVRScopeServices_SGX540_120 \
+	libsrv_init_SGX540_120 \
+	libsrv_um_SGX540_120 \
+	libusc_SGX540_120 \
+	powervr
+	
